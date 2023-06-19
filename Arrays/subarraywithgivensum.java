39f47eb82ae691d6d0b5ebbbb4440411109a84d9
AYUSH <ayushabhinav007@gmail.com>
2023-06-19 12:09:46 +0530

    fifth commit

---
+++ Arrays/subarraywithgivensum.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class subarraywithgivensum {
+
+    //naive approach with time complexity O(n square)
+    /*static boolean Subaru(int arr[], int sum){
+        
+        for(int i=0;i<arr.length;i++){
+            int cur_sum=0;
+            for(int j=i;j<arr.length;j++){
+                cur_sum+=arr[j];
+                if(cur_sum==sum){
+                    return true;
+                }
+            }
+
+        }
+        return false;*/
+
+        //efficient approach for the solution with time complexity O(1)
+        static boolean Subaru(int arr[],int sum){
+            int s=0;
+            int cur=0;
+            for(int i=0;i<arr.length;i++){
+                cur+=arr[i];
+                while(cur>sum){
+                    cur-=arr[s];
+                    s++;
+
+
+                }
+                if(cur==sum){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n =sc.nextInt();
+        int newa[]=new int[n];
+        int sum=sc.nextInt();
+        for(int i=0;i<n;i++){
+            newa[i]=sc.nextInt();
+        }
+        System.out.println(Subaru(newa, sum));
+    }
+    
+}
