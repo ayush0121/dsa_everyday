2076d06bac3dd24ad269dbc65cb402499d017ff7
Ayush Abhinav <98532012+ayush0121@users.noreply.github.com>
2024-08-11 19:28:15 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (70.28%) - LeetSync

---
+++ 541-reverse-string-ii/reverse-string-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String reverseStr(String s, int k) {
+        char[] str = s.toCharArray();
+        int n = str.length;
+        for(int i = 0 ; i <= n-1; i += 2*k){
+            if(i+k-1 <= n-1){
+                reverseK(i,i+k-1,str);
+            }else{
+				//for fewer than k characters left (edge case)
+                reverseK(i,n-1,str);
+            }
+        }
+        String ans = new String(str);
+        return ans;
+    }
+    public void reverseK(int i, int j, char[] str){
+        while(i < j){
+            char temp = str[i];
+            str[i] = str[j];
+            str[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
