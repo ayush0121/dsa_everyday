357136b3a9a2a0b015cfa493b8736c559f4d3d43
NullPointer <ayushabhinav007@gmail.com>
2024-09-20 13:00:55 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (81.72%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int n =matrix.length;
+        int n=matrix.length;
         int m =matrix[0].length;
         int low=0;
         int high=n*m-1;
@@ -11,7 +11,7 @@ class Solution {
             if(matrix[row][col]==target){
                 return true;
             }
-            else if(matrix[row][col]>target){
+            else if (matrix[row][col]>target){
                 high=mid-1;
             }else{
                 low=mid+1;
