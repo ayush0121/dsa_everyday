fec7b6b8799722f9ca4ecc3bcd91b7db726e9da2
NullPointer <ayushabhinav007@gmail.com>
2024-09-26 01:20:37 +0530

    Time: 14 ms (18.14%) | Memory: 58.7 MB (70.56%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        Stack<Integer> st=new Stack<>();
+        ListNode curr=head;
+        while(curr!=null){
+            st.push(curr.val);
+            curr=curr.next;
+        }
+        curr=head;
+        while(curr.next!=null){
+            if(st.pop()!=curr.val){
+                return false;
+            }
+            curr=curr.next;
+        }
+        return true;
+        
+        
+    }
+}
