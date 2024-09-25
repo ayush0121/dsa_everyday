d7857c3da82a7ba887518614ed0d710e57967e05
NullPointer <ayushabhinav007@gmail.com>
2024-09-25 21:56:46 +0530

    Time: 12 ms (5.04%) | Memory: 54.3 MB (5.58%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,12 +1,15 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-         int n = nums.length;
-        for (int i = 0; i < n - 1; i += 2) {
-            if (nums[i] != nums[i + 1]) {
-                return nums[i];
+        HashMap<Integer, Integer> map=new HashMap<>();
+        for(int num:nums){
+            map.put(num, map.getOrDefault(num,0)+1);
+        }
+        for(int num:nums){
+            if(map.get(num)==1){
+                return num;
             }
         }
-        return nums[n - 1];
+        return 0;
         
         
     }
