6e8f438d3e469c19a6d2cd306c37d28cb8f186b9
NullPointer <ayushabhinav007@gmail.com>
2024-10-30 17:58:30 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (19.43%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    int dp[][];
+    public int rob(int[] nums) {
+        if(nums.length == 1)return nums[0];
+        
+        dp = new int[nums.length+1][2];
+        for(int i[] : dp)Arrays.fill(i,-1);
+        
+        int one = helper(nums,0,nums.length-2,0);
+        int two = helper(nums,1,nums.length-1,1);
+        
+        return Math.max(one,two);
+    }
+    
+    public int helper(int nums[],int start,int n,int idx){
+        if(n < start)return 0;
+        if(n == start)return dp[n][idx] = nums[start];
+        if(dp[n][idx] != -1)return dp[n][idx];
+        return dp[n][idx] = Math.max(helper(nums,start,n-1,idx),helper(nums,start,n-2,idx) + nums[n]);
+    }
+}
