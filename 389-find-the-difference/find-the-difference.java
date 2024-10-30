09fdd9516b29f20f553dfdeb927080cddeca615a
NullPointer <ayushabhinav007@gmail.com>
2024-10-30 18:12:08 +0530

    Time: 1 ms (100.00%) | Memory: 41.8 MB (29.18%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -2,18 +2,13 @@ class Solution {
     public char findTheDifference(String s, String t) {
         int sum1=0;
         int sum2=0;
-        for(int i=0;i<s.length();i++){
-            int ascii1=(int) s.charAt(i);
-            sum1+=ascii1;
+        for(char c: s.toCharArray()){
+            sum1+=(int) c;
         }
-
-        for(int j=0;j<t.length();j++){
-            int ascii2=(int) t.charAt(j);
-            sum2+=ascii2;
+        for(char c:t.toCharArray()){
+            sum2+=(int) c;
         }
-        int complement=sum2-sum1;
-        char c=(char) complement;
-        return c; 
+        return (char) (sum2-sum1);
         
     }
 }
