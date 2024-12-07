23f08e4edcfa4f9dd5f3c8b3d4ed2faf82fcbc09
NullPointer <ayushabhinav007@gmail.com>
2024-12-08 05:18:38 +0530

    Time: 16 ms (94.92%) | Memory: 55.3 MB (17.34%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.java
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -1,18 +1,18 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-        Arrays.sort(people);
-        int i=0;
-        int j=people.length-1;
         int count=0;
+        Arrays.sort(people);
+        int i=0,j=people.length-1;
         while(i<=j){
             if(people[i]+people[j]<=limit){
                 i++;
-                j--;
-                count++;
-            }else{
-                count++;
-                j--;
+
             }
+                
+                j--;
+                count++;
+                
+            
         }
         return count;
         
